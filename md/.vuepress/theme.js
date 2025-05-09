b6dafeabee615f9c6d65bcb7b5265459b10ba313
lxs <liuxs@se-iot.cn>
2025-05-09 18:46:18 +0800

    feat；目录结构修改 feat：添加幻灯片

--- md/.vuepress/theme.js
+++ md/.vuepress/theme.js
@@ -15,15 +15,27 @@ export default hopeTheme({
             collapsible: true, // 启用折叠功能
             collapsed: true, // 默认折叠
             text: "写写",
-            prefix: "/content/",
+            prefix: "/content/write/",
             children: [
                 {
                     text: "VuePress 部署指南", 
                     link: "vuepress部署"
                 },
                 {
+                    text: "资源收集",
+                    link: "sources"
+                },
+                {
+                    text: "笑话收集",
+                    link: "laugh"
+                },
+                {
                     text: "《This is Water》读后感",  
                     link: "this is water 读后感"
+                },
+                {
+                    text: "踩坑记录",
+                    link: "operationRecord"
                 },
                 {
                     text: "坏心情记录",  
@@ -39,79 +51,99 @@ export default hopeTheme({
         {
             collapsible: true, // 启用折叠功能
             collapsed: true, // 默认折叠
-            text: "画画",
-            prefix: "/content/",
+            text: "烂糟画",
+            prefix: "/content/draw/",
             children: [
                 {
-                    text: "绘画作品",  
+                    text: "小画",  
                     link: "draw"
                 },
+                {
+                    text: "大画",
+                    link: "bigDraw"
+                }
             ],
         },
         {
             collapsible: true, // 启用折叠功能
             collapsed: true, // 默认折叠
             text: "小工具",
-            prefix: "/content/",
+            prefix: "/content/tool/",
             children: [
                 {
                     text: "抱抱", 
                     link: "hug"
                 },
+                {
+                    text: "幻灯片",
+                    prefix: "slide/",
+                    children: [
+                        {
+                            text: "幻灯片",
+                            link: "hdp1"
+                        }
+                    ]
+                }
             ],
         }
     ], 
-    navbar: [{
-        text: "首页",
-        link: "/"
-    },
-    {
-        text: "觀點",
-        items: [
-            {
-                text: "faith",
-                link: "/content/faith"
-            },
-            {
-                text: "this is water 读后感",
-                link: "/content/this is water 读后感"
-            },
-            {
-                text: "vuepress部署",
-                link: "/content/vuepress部署",
-            },
-            {
-                text: "资源",
-                link: "/content/sources"
-            },
-            {
-                text: "haha",
-                link: "/content/laugh"
-            },
-            {
-                text: "badMood",
-                link: "/content/badMood"
-            },
-            {
-                text: "operationRecord",
-                link: "/content/operationRecord"
-            },
-            {
-                text: "peoples",
-                link: "/content/peoples"
-            }
-        ]
-    }, {
-        text: "画廊",
-        link: "/content/draw"
-    }, {
-        text: "小工具",
-        items: [
-            {
-                text: "hug",
-                link: "/content/hug"
-            }
-        ]
-    }
-    ]
+    // navbar: [{
+    //     text: "首页",
+    //     link: "/"
+    // },
+    // {
+    //     text: "觀點",
+    //     items: [
+    //         {
+    //             text: "faith",
+    //             link: "/content/faith"
+    //         },
+    //         {
+    //             text: "this is water 读后感",
+    //             link: "/content/this is water 读后感"
+    //         },
+    //         {
+    //             text: "vuepress部署",
+    //             link: "/content/vuepress部署",
+    //         },
+    //         {
+    //             text: "资源",
+    //             link: "/content/sources"
+    //         },
+    //         {
+    //             text: "haha",
+    //             link: "/content/laugh"
+    //         },
+    //         {
+    //             text: "badMood",
+    //             link: "/content/badMood"
+    //         },
+    //         {
+    //             text: "operationRecord",
+    //             link: "/content/operationRecord"
+    //         },
+    //         {
+    //             text: "peoples",
+    //             link: "/content/peoples"
+    //         }
+    //     ]
+    // }, {
+    //     text: "画廊",
+    //     link: "/content/draw"
+    // }, {
+    //     text: "小工具",
+    //     items: [
+    //         {
+    //             text: "hug",
+    //             link: "/content/hug"
+    //         }
+    //     ]
+    // }
+    // ],
+    markdown: {
+        revealjs: true,
+      }
 })
+
+
+ 
---
+++ md/content/tool/slide/hdp1.md
@@ -0,0 +1,27 @@
+---
+layout: Slides
+---
+
+@slidestart
+
+<!-- 此处是幻灯片内容 -->
+
+## 幻灯片标题
+
+一个拥有文字和 [链接](https://mister-hope.com) 的段落
+
+---
+
+## 代码高亮
+
+```js [2-4|1-5]
+const add = (a, b) => {
+  if (typeof b === "undefined") return a + 1;
+
+  return a + b;
+};
+```
+
+
+
+@slideend
