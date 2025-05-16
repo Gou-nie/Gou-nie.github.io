b0844457c1f84568626cbcfc44a4ee5b0a45dc3c
lxs <liuxs@se-iot.cn>
2025-05-16 16:42:35 +0800

    feat：three.js初始学习提交

--- md/.vuepress/components/ThreeTest.vue
+++ md/.vuepress/components/ThreeTest.vue
@@ -1,45 +1,132 @@
 <template>
-    <div>
-      <canvas ref="threeCanvas"></canvas>
-    </div>
-  </template>
-  
-  <script>
-  import * as THREE from 'three'
-  export default {
-    mounted() {
-      this.initThree()
-    },
-    methods: {
-      initThree() {
-        const scene = new THREE.Scene()
-        scene.background = new THREE.Color('#eee')
-        const canvas = this.$refs.threeCanvas
-        const renderer = new THREE.WebGLRenderer({ canvas, antialias: true })
-        const camera = new THREE.PerspectiveCamera(
-          50,
-          window.innerWidth / window.innerHeight,
-          0.1,
-          1000
-        )
-        camera.position.z = 10
-  
-        function animate() {
-          renderer.render(scene, camera)
-          requestAnimationFrame(animate)
+  <div>
+    <canvas class="threeCanvas" ref="threeCanvas"></canvas>
+  </div>
+</template>
+
+<script>
+import * as THREE from 'three'
+export default {
+  mounted() {
+    this.initThree()
+  },
+  methods: {
+    initThree() {
+      const scene = new THREE.Scene()
+      scene.background = new THREE.Color('#aaa')
+      const canvas = this.$refs.threeCanvas
+      const renderer = new THREE.WebGLRenderer({ canvas, antialias: true })
+      const camera = new THREE.PerspectiveCamera(
+        25, // fov 视野范围(field of view)的缩写 垂直方向角度
+        2, // 画布宽高比
+        0.1, // near 近平面最小值
+        12 // far 远平面最大值
+      )
+      camera.position.z = 10
+
+      //光照
+      const color = 0xFFFFFF;
+      const intensity = 3;
+      const light = new THREE.DirectionalLight(color, intensity);
+      light.position.set(-1, 2, 4);
+      scene.add(light);
+
+      // 创建立方体
+      const boxWidth = 1;
+      const boxHeight = 1;
+      const boxDepth = 1;
+      const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+      const material1 = new THREE.MeshPhongMaterial({ color: 0xbbaaee });
+      const cube1 = new THREE.Mesh(geometry, material1);
+      scene.add(cube1);
+
+      const material2 = new THREE.MeshPhongMaterial({ color: 0xbbffee });
+      const cube2 = new THREE.Mesh(geometry, material2);
+      cube2.position.x = 1.5; // 向右移动1.5u
+      scene.add(cube2);
+      renderer.render(scene, camera)
+
+
+      function resizeRendererToDisplaySize(renderer) {
+        const canvas = renderer.domElement;
+        const width = canvas.clientWidth;
+        const height = canvas.clientHeight;
+        const needResize = canvas.width !== width || canvas.height !== height;
+        if (needResize) {
+          renderer.setSize(width, height, false);
         }
-        animate()
-      },
+        return needResize;
+      }
+
+
+      // 定时器更新动画
+      function rerrr(time) {
+        time *= 0.0001;
+
+        cube1.rotation.x = time;
+        cube1.rotation.y = time;
+        cube2.rotation.x = 1.5 * time;
+        cube2.rotation.y = 1.5 * time;
+
+        const canvas = renderer.domElement;
+        camera.aspect = canvas.clientWidth / canvas.clientHeight;
+        camera.updateProjectionMatrix();
+
+        if (resizeRendererToDisplaySize(renderer)) {
+          const canvas = renderer.domElement;
+          camera.aspect = canvas.clientWidth / canvas.clientHeight;
+          camera.updateProjectionMatrix();
+        }
+
+
+        renderer.render(scene, camera);
+
+        requestAnimationFrame(rerrr);
+      }
+      rerrr();
+      // requestAnimationFrame(rerrr);
+
+
+
+
     },
-  }
-  </script>
-  
-  <style scoped>
-  #threeCanvas {
-    width: 100%;
-    height: 100%;
-    position: fixed;
-    left: 0;
-    top: 0;
-  }
-  </style>
+    /**
+     * const loader = new THREE.FontLoader();
+
+loader.load('../resources/threejs/fonts/helvetiker_regular.typeface.json', (font) => {
+  const text = 'aleahpeal';  
+aleahpeal
+
+  const geometry = new THREE.TextGeometry(text, {
+    font: font,
+    size:  2.3,  
+
+    depth:  3.5,  
+
+    curveSegments: 12,  
+
+    bevelEnabled: true,  
+    bevelThickness: 0.77,  
+
+    bevelSize: 0.30,  
+
+    bevelSegments: 8,  
+
+  });  
+     */
+  },
+}
+</script>
+
+<style scoped>
+.threeCanvas {
+  /* 修改ID选择器为class选择器 */
+  width: 100%;
+  height: 400px;
+  /* 设置固定高度 */
+  display: block;
+  margin: 20px auto;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+}
+</style>
