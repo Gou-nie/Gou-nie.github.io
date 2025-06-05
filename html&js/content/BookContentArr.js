6e79f11c316dc58b47b268f27788f61f1962f342
Gou-nie <Gou-nie@users.noreply.github.com>
2025-06-05 03:43:46 +0000

    deploy: 8c67a225dbdd7823af36c7e1740168872913260f

---
+++ html&js/content/BookContentArr.js
@@ -0,0 +1,106 @@
+const bookArr = [
+    {
+      fileUrl: "/models/spellbook.glb",
+      name: "book1",
+      url: "content/draw/bigDraw",
+      title: "大图"
+    },
+    {
+      fileUrl: "/models/spellbook.glb",
+      name: "book2",
+      url: "content/draw/draw",
+      title: "画画"
+    },
+    {
+      fileUrl: "/models/spellbook.glb",
+      name: "book3",
+      url: "content/tool/fluid",
+      title: "流体"
+    },
+    {
+      fileUrl: "/models/spellbook.glb",
+      name: "book4",
+      url: "content/tool/hug",
+      title: "拥抱"
+    },
+    {
+      fileUrl: "/models/spellbook.glb",
+      name: "book5",
+      url: "content/tool/three/shelf3D",
+      title: "书架"
+    },
+    {
+      fileUrl: "/models/spellbook.glb",
+      name: "book6",
+      url: "content/tool/three/testThree",
+      title: "3D测试"
+    },
+    {
+      fileUrl: "/models/spellbook.glb",
+      name: "book7",
+      url: "content/vuepress部署",
+      title: "vuepress部署"
+    },
+    {
+      fileUrl: "/models/spellbook.glb",
+      name: "book8",
+      url: "content/write/badMood",
+      title: "坏心情"
+    },
+    {
+      fileUrl: "/models/spellbook.glb",
+      name: "book9",
+      url: "content/write/disenchantment",
+      title: "祛魅"
+    },
+    {
+      fileUrl: "/models/spellbook.glb",
+      name: "book10",
+      url: "content/write/faith",
+      title: "信仰"
+    },
+    {
+      fileUrl: "/models/spellbook.glb",
+      name: "book11",
+      url: "content/write/iosPriBlue",
+      title: "ios私有蓝牙协议"
+    },
+    {
+      fileUrl: "/models/spellbook.glb",
+      name: "book12",
+      url: "content/write/love",
+      title: "爱"
+    },
+    {
+      fileUrl: "/models/spellbook.glb",
+      name: "book13",
+      url: "content/write/operationRecord",
+      title: "操作记录"
+    },
+    {
+      fileUrl: "/models/spellbook.glb",
+      name: "book14",
+      url: "content/write/peoples",
+      title: "人物"
+    },
+    {
+      fileUrl: "/models/spellbook.glb",
+      name: "book15",
+      url: "content/write/songs",
+      title: "歌曲"
+    },
+    {
+      fileUrl: "/models/spellbook.glb",
+      name: "book16",
+      url: "content/write/sources",
+      title: "推荐"
+    },
+    {
+      fileUrl: "/models/spellbook.glb",
+      name: "book17",
+      url: "content/write/this is water 读后感",
+      title: "读后感"
+    },
+  ];
+
+export default bookArr;
--- html&js/three3D/ThreeStrFunc.js
+++ html&js/three3D/ThreeStrFunc.js
@@ -3,55 +3,90 @@ import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
 
 // 创建文字面板
-export function createTextPlane(text = "Hello Three.js", width = 2, height = 1) {
-    // 1. 创建 Canvas
-    const canvas = document.createElement('canvas');
-    const context = canvas.getContext('2d');
-    canvas.width = 128;
-    canvas.height = 512;
+// 竖向文字
+export function createVerticalTextPlane(text = "Hello Three.js", width = 1, height = 2) {
+  // 1. 创建 Canvas
+  const canvas = document.createElement('canvas');
+  const context = canvas.getContext('2d');
+  canvas.width = 128;
+  canvas.height = 512;
 
-    // 2. 绘制背景
-    context.fillStyle = '#ffffff';
-    context.fillRect(0, 0, canvas.width, canvas.height);
+  // 2. 绘制背景
+  context.fillStyle = '#ffffff';
+  context.fillRect(0, 0, canvas.width, canvas.height);
 
-    // 3. 绘制文本
-    context.font = 'bold 48px Arial';
-    context.fillStyle = '#000000';
-    context.textAlign = 'center';
+  // 3. 绘制文本
+  context.font = 'bold 48px Arial';
+  context.fillStyle = '#000000';
+  context.textAlign = 'center';
 
-    // 横向
-    //   context.textBaseline = 'middle';
-    //   context.fillText(text, canvas.width / 2, canvas.height / 2);
+  // 横向
+  //   context.textBaseline = 'middle';
+  //   context.fillText(text, canvas.width / 2, canvas.height / 2);
 
-    // 竖向
-    context.textBaseline ='top';
-    const x = canvas.width / 2;
-    let y = 20;
-    const lineHeight = 48;
-    for (let i = 0; i < text.length; i++) {
-        context.fillText(text[i], x, y);
-        y += lineHeight;
-    }
-    // 4. 转换为纹理
-    const texture = new THREE.CanvasTexture(canvas);
-    texture.needsUpdate = true;
+  // 竖向
+  context.textBaseline = 'top';
+  const x = canvas.width / 2;
+  let y = 20;
+  const lineHeight = 48;
+  for (let i = 0; i < text.length; i++) {
+    context.fillText(text[i], x, y);
+    y += lineHeight;
+  }
+  // 4. 转换为纹理
+  const texture = new THREE.CanvasTexture(canvas);
+  texture.needsUpdate = true;
 
-    // 5. 创建材质和平面几何体
-    const material = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide });
-    const geometry = new THREE.PlaneGeometry(width, height);
-    const mesh = new THREE.Mesh(geometry, material);
+  // 5. 创建材质和平面几何体
+  const material = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide });
+  const geometry = new THREE.PlaneGeometry(width, height);
+  const mesh = new THREE.Mesh(geometry, material);
 
-    return mesh;
+  return mesh;
+}
+// 横向文字
+export function createHorizontalTextPlane(text = "Hello Three.js", width = 2, height = 1) {
+  // 1. 创建 Canvas
+  const canvas = document.createElement('canvas');
+  const context = canvas.getContext('2d');
+  canvas.width = 512;
+  canvas.height = 256;
+
+  // 2. 绘制背景
+  context.fillStyle = '#ffffff';
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  // 3. 绘制文本
+  context.font = 'bold 48px Arial';
+  context.fillStyle = '#000000';
+  context.textAlign = 'center';
+
+  // 横向
+  context.textBaseline = 'middle';
+  context.fillText(text, canvas.width / 2, canvas.height / 2);
+  // 4. 转换为纹理
+  const texture = new THREE.CanvasTexture(canvas);
+  texture.needsUpdate = true;
+
+  // 5. 创建材质和平面几何体
+  const material = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide });
+  const geometry = new THREE.PlaneGeometry(width, height);
+  const mesh = new THREE.Mesh(geometry, material);
+
+  return mesh;
+}
+// 花式面板
+export function createGlamorousTextPlane(text = "Hello Three.js", width = 1, height = 2) {
+  
 }
 
 
 
-
 // 创建3D文字
-export function createTextMesh(text, x, y, z, signal, fontUrl = '../../fonts/undefined_Regular.json') {
+export function createTextMesh(text, x, y, z, signal, fontUrl = '../../fonts/helvetiker_regular.typeface.json') {
   return new Promise((resolve, reject) => {
-    console.log(signal,"---------signal is ");
-    if(signal?.aborted){
+    console.log(signal, "---------signal is ");
+    if (signal?.aborted) {
       reject(new Error('Aborted before start'));
       return;
     }
@@ -59,7 +94,7 @@ export function createTextMesh(text, x, y, z, signal, fontUrl = '../../fonts/und
     loader.load(fontUrl, (font) => {
 
 
-      if(signal?.aborted){
+      if (signal?.aborted) {
         reject(new Error('Aborted during loading'));
         return;
       }
@@ -81,7 +116,7 @@ export function createTextMesh(text, x, y, z, signal, fontUrl = '../../fonts/und
       mesh.position.set(x, y, z);
       resolve(mesh);
     }, undefined, reject);
-    
+
     signal?.addEventListener('abort', () => {
       reject(new Error('Aborted by signal'));
     });
