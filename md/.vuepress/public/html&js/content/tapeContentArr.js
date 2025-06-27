64226b6170a0d6216c7ba8513953bb537eb877f1
lxs <liuxs@se-iot.cn>
2025-06-27 11:45:22 +0800

    ff 链接->静态文件

--- md/.vuepress/public/html&js/content/tapeContentArr.js
+++ md/.vuepress/public/html&js/content/tapeContentArr.js
@@ -2,10 +2,12 @@
 const tapeArr=[
     {
         name: "tape1",
-        url: "https://ia803105.us.archive.org/33/items/LetItBe1/Let%20it%20be%20%281%29.mp3"
+        url:"/music/LetItBe.mp3"
+        // url: "https://ia803105.us.archive.org/33/items/LetItBe1/Let%20it%20be%20%281%29.mp3"
     },{
         name: "tape2",
-        url:"https://dn721908.ca.archive.org/0/items/youtube-yl3TsqL0ZPw/yl3TsqL0ZPw.mp4"
+        // url:"https://dn721908.ca.archive.org/0/items/youtube-yl3TsqL0ZPw/yl3TsqL0ZPw.mp4"
+        url:"/music/BohemianRhapsody.mp4"
     }
 ]
 
