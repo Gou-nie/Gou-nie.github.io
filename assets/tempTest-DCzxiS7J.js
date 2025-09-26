c795cc4b977ed63463cc55ce364aca343f158d55
Gou-nie <Gou-nie@users.noreply.github.com>
2025-09-26 10:42:08 +0000

    deploy: 592830359e611a179d49f6b568991bbc68d3fed5

---
+++ assets/tempTest-DCzxiS7J.js
@@ -0,0 +1,1 @@
+import e from"./FloatingBall-cRVDlo7r.js";import{x as t,c as o,o as s,d as a}from"./app-qP7LBjAK.js";import{_ as c}from"./plugin-vue_export-helper-DlAUqK2U.js";const r={class:"home-view"},_=t({__name:"tempTest",setup(m){return(n,p)=>(s(),o("div",r,[a(e)]))}}),l=c(_,[["__scopeId","data-v-cd0c23f9"]]);export{l as default};
