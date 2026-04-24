425b79483eb9cfe26d156a784900ca57fefcbb0e
Gou-nie <Gou-nie@users.noreply.github.com>
2026-04-24 08:32:51 +0000

    deploy: cb802df662afba5ebb900431f110a85bb7543f16

---
+++ assets/backHome26.html-CJ770k-t.js
@@ -0,0 +1,28 @@
+import{_ as s}from"./plugin-vue_export-helper-DlAUqK2U.js";import{c as a,i,o as e}from"./app-CTKn_6cl.js";const l={};function p(r,n){return e(),a("div",null,n[0]||(n[0]=[i(`<h1>26新年回家过年列车上</h1><h1 id="_26新年回家过年列车上" tabindex="-1"><a class="header-anchor" href="#_26新年回家过年列车上"><span>26新年回家过年列车上</span></a></h1><p><em><strong>听歌听到那天听到的新歌，想起来在那趟回家之旅</strong></em></p><ul><li><ol><li><a href="#first">高铁</a></li></ol></li><li><ol start="2"><li><a href="#second">xx2</a></li></ol></li></ul><h2 id="_1-高铁" tabindex="-1"><a class="header-anchor" href="#_1-高铁"><span>1. <a name="first"></a> 高铁</span></a></h2><p><br>  <br>  这次买到的票是6h的站票，以为不想再为后边的放票劳神就直接选了这个，相比第一年站14h的绿皮还是很好的。上了高铁就选定了一个门口靠在那里看窗外。在浙江这一段都是二三十分钟一站，开开合合我也厌烦就带着包站到了对面的门口。恰巧走到了一段桥上，这个桥里水面很近，有点海上列车的感觉。看了下地图才知道是一个湖，湖面好多平纹，我这面太阳晒着波光鳞鳞，对面那一面有山和云遮着映出游着小白线的深蓝帆布。幻想着列车停下打开门，然后我们下车去水上走一走，踩着软软的水面，晒晒太阳，捞捞水草。我正想着跑到那一块养殖池看看是什么鱼的时候葱葱的绿幕拉下这场幻戏。南方这边真好，树木草地处处长青。 <br>  <br>  一波人下去，一波人上来，车门开了又关，关了又开，歌单听了一遍又一遍，关闭论坛的页面，打开随机推荐。抬头时才发现窗外已经没有绿绿的水潭和山丘了，取而代之的是刚冒头的麦地和偶尔划过的一条河流。看着整齐的田垄在车窗上一遍遍刷着，耳机里两声弦渐弱的拨动引出鼓敲打着简单而缓慢的节奏，间隙中插着不知是砂锤还是什么的乐器。我好像窗外的掠过的影子跟这音乐一起飘荡。一个女声念着歌词跟着这个节奏一起慢慢出来，重复着简单的节奏，给我很熟悉的感觉，看着出现有些黄草的枯地，总觉得要回忆出什么。可直到这首歌完了也没想起来那种熟悉从何而来。没多久太阳落到了树梢，有些小气，把红光全部收载自己的身体里，周围的天空还是由白色慢慢转到淡蓝。 <br> 对了 那首歌叫：《远远的远 远远》歌词如下：</p><div class="language- line-numbers-mode" data-highlighter="shiki" data-ext="" style="--shiki-light:#383A42;--shiki-dark:#abb2bf;--shiki-light-bg:#FAFAFA;--shiki-dark-bg:#282c34;"><pre class="shiki shiki-themes one-light one-dark-pro vp-code"><code><span class="line"><span></span></span>
+<span class="line"><span>路上的叶 满天</span></span>
+<span class="line"><span>天上的山 隐约</span></span>
+<span class="line"><span>脚下的雨 呻吟</span></span>
+<span class="line"><span>眼中的湖 是你</span></span>
+<span class="line"><span>轻轻地飞 哪里</span></span>
+<span class="line"><span>红红的叶 满天</span></span>
+<span class="line"><span>长长的线 有限</span></span>
+<span class="line"><span>远远的远 远远</span></span>
+<span class="line"><span>鸡叫的声 很远</span></span>
+<span class="line"><span>清晨的风 很疼</span></span>
+<span class="line"><span>要嫁的人 不在</span></span>
+<span class="line"><span>烧着的水 太慢</span></span>
+<span class="line"><span>轻轻地飞 哪里</span></span>
+<span class="line"><span>红红的叶 满天</span></span>
+<span class="line"><span>长长的线 有限</span></span>
+<span class="line"><span>远远的远 远远</span></span>
+<span class="line"><span>music…………</span></span>
+<span class="line"><span>鸡叫的声 很远</span></span>
+<span class="line"><span>清晨的风 很疼</span></span>
+<span class="line"><span>要嫁的人 不在</span></span>
+<span class="line"><span>烧着的水 太慢</span></span>
+<span class="line"><span>鸡叫的声 很远</span></span>
+<span class="line"><span>清晨的风 很疼</span></span>
+<span class="line"><span>要嫁的人 不在</span></span>
+<span class="line"><span>烧着的水 太慢</span></span>
+<span class="line"><span>嗒嗒嗒……</span></span>
+<span class="line"><span>嗒嗒嗒……</span></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,7)]))}const t=s(l,[["render",p]]),m=JSON.parse('{"path":"/content/write/backHome26.html","title":"26新年回家过年列车上","lang":"en-US","frontmatter":{"title":"26新年回家过年列车上","tags":["write"],"category":["write"]},"git":{"createdTime":1772704932000,"updatedTime":1772704932000,"contributors":[{"name":"lxs","username":"lxs","email":"liuxs@se-iot.cn","commits":1,"url":"https://github.com/lxs"}]},"readingTime":{"minutes":2.65,"words":795},"filePathRelative":"content/write/backHome26.md","excerpt":" \\n<h1>26新年回家过年列车上</h1>\\n<p><em><strong>听歌听到那天听到的新歌，想起来在那趟回家之旅</strong></em></p>\\n<ul>\\n<li>\\n<ol>\\n<li><a href=\\"#first\\">高铁</a></li>\\n</ol>\\n</li>\\n<li>\\n<ol start=\\"2\\">\\n<li><a href=\\"#second\\">xx2</a></li>\\n</ol>\\n</li>\\n</ul>\\n<h2><a class=\\"header-anchor\\" href=\\"#_1-高铁\\"><span>1. </span></a><a name=\\"first\\"></a> 高铁</h2>"}');export{t as comp,m as data};
---
+++ assets/tempTest-HzXSsr0p.js
@@ -0,0 +1,1 @@
+import e from"./FloatingBall-BN5tBMZy.js";import{B as t,c as o,o as s,f as a}from"./app-CTKn_6cl.js";import{_ as c}from"./plugin-vue_export-helper-DlAUqK2U.js";const r={class:"home-view"},_=t({__name:"tempTest",setup(m){return(n,p)=>(s(),o("div",r,[a(e)]))}}),l=c(_,[["__scopeId","data-v-cd0c23f9"]]);export{l as default};
